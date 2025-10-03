5852a44e102a7640a70d56c0b32cd94fc906b92a
kingAI-bit <sharnuke.asrilsyak@lecturer.unri.ac.id>
2025-10-03 22:22:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle menu untuk mobile
+document.getElementById('menu-btn').addEventListener('click', function () {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('hidden');
+});
